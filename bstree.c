512e080aad99050f9ee7d309f9272b3e154e7e57
hq <602997434@qq.com>
2021-05-29 07:16:59 -0700

    bstree add

---
+++ bstree.c
@@ -0,0 +1,70 @@
+
+//二叉搜索树
+
+typedef int KEY_VALUE
+
+#define BSTREE_ENTRY(name, type) \
+        struct name {            \
+            struct type *left;   \
+            struct type *right;  \
+        }
+
+struct bstree_node {
+    BSTREE_ENTRY(, bstree_node) bst;
+
+    KEY_VALUE key;
+}
+
+struct bstree {
+    struct bstree_node* root;
+}
+
+struct bstree_node *bstree_create_node(KEY_VALUE key) {
+    struct bstree_node *node = (struct bstree_node)malloc(sizeof(struct bstree_node));
+    if (node == NULL) {
+        return NULL;
+    }
+
+    node->key = key;
+    node->bst.left = node->bst.right = NULL;
+
+    return node;
+}
+
+int bstree_insert(struct bstree *tree, KEY_VALUE key) {
+    if (tree == NULL) return -1;
+
+    if (tree->root == NULL) {
+        tree->root = bstree_create_node(key);
+        return 0
+    }
+
+    struct bstree_node *node = tree->root;
+    struct bstree_node *parent = tree->root;
+
+    while (node != NULL) {
+        parent = node;
+        if (key < node->key) {
+            node = node->bst.left;
+        } else if (key > node->key) {
+            node = node->bst.right;
+        } else {
+            //exists
+            return -2;
+        }
+    }
+
+    if (key < parent->key) {
+        parent->bst.left = bstree_create_node(key);
+    } else if (key > parent->key) {
+        parent->bst.right = bstree_create_node(key);
+    }
+}
+
+int bstree_traversal(struct bstree_node *node) {
+    if (node == NULL) return 0;
+
+    bstree_traversal(node->bst.left);
+    printf("%4d", node->key);
+    bstree_traversal(node->bst.right);
+}
